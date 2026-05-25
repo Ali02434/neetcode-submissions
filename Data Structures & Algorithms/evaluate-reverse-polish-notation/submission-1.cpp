86820a5e1dee63e4f344509d5460a6582a80ba14
Ali Sener <ali.sener@tu-ilmenau.de>
2026-05-25 13:24:22 +0200

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int a;
+        int b;
+
+        for(int i = 0; i < tokens.size(); i++){
+            if(tokens[i] == "+"){
+                a = st.top();st.pop();
+                b = st.top();st.pop();
+                st.push(a+b);
+            }
+            else if(tokens[i] == "-"){
+                a = st.top();st.pop();
+                b = st.top();st.pop();
+                st.push(b-a);
+            }
+            else if(tokens[i] == "*"){
+                a = st.top();st.pop();
+                b = st.top();st.pop();
+                st.push(a*b);
+            }
+            else if(tokens[i] == "/"){
+                a = st.top();st.pop();
+                b = st.top();st.pop();
+                st.push(floor(b/a));
+            }
+            else{
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
